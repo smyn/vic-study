ed14a1b4b9fb188ddfcb3df582ac0b2566b2a08d
Administrator <670204515@qq.com>
2021-08-06 21:04:04 +0800

    提价代码

---
+++ mybatis-demo/src/main/java/com/vic/spring/mybatis/mybatisdemo/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.vic.spring.mybatis.mybatisdemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.vic.spring.mybatis.mybatisdemo.model.User;
+
+
+public interface UserMapper extends BaseMapper<User> {
+}
