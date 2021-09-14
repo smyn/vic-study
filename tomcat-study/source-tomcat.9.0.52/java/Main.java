fac8ad3fddcf4e1d0874b4a6e7a66d090098d1b6
smyn <670204515@qq.com>
2021-09-14 09:58:46 +0800

    update path

--- tomcat-study/source-tomcat.9.0.52/java/Main.java
+++ tomcat-study/source-tomcat.9.0.52/java/Main.java
@@ -9,14 +9,14 @@ import org.apache.catalina.startup.Tomcat;
 public class Main {
 
     public static void main(String[] args) {
-//        -Dcatalina.home=D:/code/github/spring-demo/tomcat-study/source-tomcat.9.0.52/
-//            -Dcatalina.base=D:/code/github/spring-demo/tomcat-study/source-tomcat.9.0.52/
-//            -Djava.endorsed.dirs=D:/code/github/spring-demo/tomcat-study/source-tomcat.9.0.52/endorsed
-//            -Djava.io.tmpdir=D:/code/github/spring-demo/tomcat-study/source-tomcat.9.0.52/temp
-//            -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
-//            -Djava.util.logging.config.file=D:/code/github/spring-demo/tomcat-study/source-tomcat.9.0.52/conf/logging.properties
-//            -Duser.language=en
-//            -Duser.region=US
+//-Dcatalina.home=D:/code/github/vic-study/tomcat-study/source-tomcat.9.0.52/
+//-Dcatalina.base=D:/code/github/vic-study/tomcat-study/source-tomcat.9.0.52/
+//-Djava.endorsed.dirs=D:/code/github/vic-study/tomcat-study/source-tomcat.9.0.52/endorsed
+//-Djava.io.tmpdir=D:/code/github/vic-study/tomcat-study/source-tomcat.9.0.52/temp
+//-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
+//-Djava.util.logging.config.file=D:/code/github/vic-study/tomcat-study/source-tomcat.9.0.52/conf/logging.properties
+//-Duser.language=en
+//-Duser.region=US
         Bootstrap.main(args);
     }
 }
